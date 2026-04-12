3ae971e0da08af50f2751f3171305c4be2a80e78
qm210 <quantenmace@gmail.com>
2026-04-12 04:15:28 +0200

    more builtin functions

--- Traverser.h
+++ Traverser.h
@@ -15,31 +15,132 @@ using namespace glslang;
 static const char* builtinName(TOperator op) {
     switch (op) {
         case EOpMod: return "mod";
-        case EOpModf: return "modf";
+        case EOpRadians: return "radians";
+        case EOpDegrees: return "degrees";
         case EOpSin: return "sin";
         case EOpCos: return "cos";
         case EOpTan: return "tan";
         case EOpAsin: return "asin";
         case EOpAcos: return "acos";
         case EOpAtan: return "atan";
+        case EOpSinh: return "sinh";
+        case EOpCosh: return "cosh";
+        case EOpTanh: return "tanh";
+        case EOpAsinh: return "asinh";
+        case EOpAcosh: return "acosh";
+        case EOpAtanh: return "atanh";
+        case EOpPow: return "pow";
+        case EOpExp: return "exp";
+        case EOpLog: return "log";
+        case EOpExp2: return "exp2";
+        case EOpLog2: return "log2";
         case EOpSqrt: return "sqrt";
         case EOpInverseSqrt: return "inversesqrt";
         case EOpAbs: return "abs";
         case EOpSign: return "sign";
         case EOpFloor: return "floor";
+        case EOpTrunc: return "trunc";
+        case EOpRound: return "round";
+        case EOpRoundEven: return "roundEven";
         case EOpCeil: return "ceil";
         case EOpFract: return "fract";
-        case EOpPow: return "pow";
-        case EOpExp: return "exp";
-        case EOpExp2: return "exp2";
-        case EOpLog: return "log";
-        case EOpLog2: return "log2";
+        case EOpModf: return "modf";
         case EOpMin: return "min";
         case EOpMax: return "max";
         case EOpClamp: return "clamp";
         case EOpMix: return "mix";
         case EOpStep: return "step";
         case EOpSmoothStep: return "smoothstep";
+        case EOpIsNan: return "isnan";
+        case EOpIsInf: return "isinf";
+        case EOpFma: return "fma";
+        case EOpFrexp: return "frexp";
+        case EOpLdexp: return "ldexp";
+/* // TODO check all these...
+    EOpFloatBitsToInt,
+    EOpFloatBitsToUint,
+    EOpIntBitsToFloat,
+    EOpUintBitsToFloat,
+    EOpDoubleBitsToInt64,
+    EOpDoubleBitsToUint64,
+    EOpInt64BitsToDouble,
+    EOpUint64BitsToDouble,
+    EOpFloat16BitsToInt16,
+    EOpFloat16BitsToUint16,
+    EOpInt16BitsToFloat16,
+    EOpUint16BitsToFloat16,
+    EOpPackSnorm2x16,
+    EOpUnpackSnorm2x16,
+    EOpPackUnorm2x16,
+    EOpUnpackUnorm2x16,
+    EOpPackSnorm4x8,
+    EOpUnpackSnorm4x8,
+    EOpPackUnorm4x8,
+    EOpUnpackUnorm4x8,
+    EOpPackHalf2x16,
+    EOpUnpackHalf2x16,
+    EOpPackDouble2x32,
+    EOpUnpackDouble2x32,
+    EOpPackInt2x32,
+    EOpUnpackInt2x32,
+    EOpPackUint2x32,
+    EOpUnpackUint2x32,
+    EOpPackFloat2x16,
+    EOpUnpackFloat2x16,
+    EOpPackInt2x16,
+    EOpUnpackInt2x16,
+    EOpPackUint2x16,
+    EOpUnpackUint2x16,
+    EOpPackInt4x16,
+    EOpUnpackInt4x16,
+    EOpPackUint4x16,
+    EOpUnpackUint4x16,
+    EOpPack16,
+    EOpPack32,
+    EOpPack64,
+    EOpUnpack32,
+    EOpUnpack16,
+    EOpUnpack8,
+
+    EOpLength,
+    EOpDistance,
+    EOpDot,
+    EOpCross,
+    EOpNormalize,
+    EOpFaceForward,
+    EOpReflect,
+    EOpRefract,
+
+    EOpMin3,
+    EOpMax3,
+    EOpMid3,
+
+    EOpDPdx,            // Fragment only
+    EOpDPdy,            // Fragment only
+    EOpFwidth,          // Fragment only
+    EOpDPdxFine,        // Fragment only
+    EOpDPdyFine,        // Fragment only
+    EOpFwidthFine,      // Fragment only
+    EOpDPdxCoarse,      // Fragment only
+    EOpDPdyCoarse,      // Fragment only
+    EOpFwidthCoarse,    // Fragment only
+
+    EOpInterpolateAtCentroid, // Fragment only
+    EOpInterpolateAtSample,   // Fragment only
+    EOpInterpolateAtOffset,   // Fragment only
+    EOpInterpolateAtVertex,
+
+    EOpMatrixTimesMatrix,
+    EOpOuterProduct,
+    EOpDeterminant,
+    EOpMatrixInverse,
+    EOpTranspose,
+
+    EOpFtransform,
+
+    EOpNoise,
+
+ */
         case EOpLength: return "length";
         case EOpDistance: return "distance";
         case EOpDot: return "dot";
